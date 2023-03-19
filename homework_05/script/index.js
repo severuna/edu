7c0ae34188bb93bf772d15020991df0b237045df
Severuna <oxxxyfomenkova@gmail.com>
2023-03-19 21:04:13 +0200

    set filterEvenNumbers func logic

--- homework_05/script/index.js
+++ homework_05/script/index.js
@@ -1,6 +1,11 @@
 // 1. Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа
 // передані як аргументи функції.
 
+
+const filterEvenNumbers = (...numbers) => {
+    return numbers.filter( el => el % 2 != 0 )
+}
+
 const filterEvenForm = document.forms.filterEvenForm;
 
 filterEvenForm.addEventListener("submit", ( e ) => {
