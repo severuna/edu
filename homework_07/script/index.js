fa461adae3617a65ac89601256b109ba32215403
Severuna <oxxxyfomenkova@gmail.com>
2023-03-25 21:20:28 +0200

    task 1 done

---
+++ homework_07/script/index.js
@@ -0,0 +1,19 @@
+// 1. Створіть 3 об'єкти, що описують країни:
+
+const ukraine = { 
+    tax: 0.195, 
+    middleSalary: 1789, 
+    vacancies: 11476 
+};
+
+const latvia = { 
+    tax: 0.25, 
+    middleSalary: 1586, 
+    vacancies: 3921 
+};
+
+const litva = { 
+    tax: 0.15, 
+    middleSalary: 1509, 
+    vacancies: 1114 
+};
