bb64385c41da6b6635ca1828a953fe37c40a58fe
Severuna <oxxxyfomenkova@gmail.com>
2023-03-26 23:05:01 +0300

    task 1 done - create class Student with prop

--- homework_08/script/index.js
+++ homework_08/script/index.js
@@ -1,1 +1,11 @@
 // 1. У цьому завданні вам необхідно створити клас Student та розробляти методи всередині цього класу. У стдентів повинні бути наступні властивості: university, course, fullName, вони передаються при створенні студента(в конструктор).
+
+class Student {
+
+    constructor( university, course, fullName ) {
+        this.university = university;
+        this.course = course;
+        this.fullName = fullName;
+    }
+    
+}
