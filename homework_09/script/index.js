9901c188c5d00767de17e76acf67fa7c22ab153d
Severuna <oxxxyfomenkova@gmail.com>
2023-03-31 23:23:46 +0300

    set generateBlocks func logic

--- homework_09/script/index.js
+++ homework_09/script/index.js
@@ -1,1 +1,13 @@
-const root = document.querySelector("#root");
+document.addEventListener("DOMContentLoaded", ( ) => {
+    const root = document.querySelector("#root");
+
+    const generateBlocks = ( container ) => {
+        for ( let i = 0; i < 25 ; i++ ) {
+            const item = document.createElement("div");
+            item.classList.add("item");
+            item.style.backgroundColor = `rgb(${Math.floor( Math.random() * ( 250 - 0 ) + 0 )}, ${Math.floor( Math.random() * ( 250 - 0 ) + 0 )}, ${Math.floor( Math.random() * ( 250 - 0 ) + 0 )})`;
+            container.append(item);
+        }
+    }
+    generateBlocks(root);
+});
