771126ad74ff1a944b22c9b43b4824830d7a4431
Severuna <oxxxyfomenkova@gmail.com>
2023-04-08 21:41:20 +0300

    set getRandomChinese func logic & output example in concole

--- homework_11/script/index.js
+++ homework_11/script/index.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", ( ) => {
-    // init mess
-    console.log(`the page is loaded`);
+    
+    async function getRandomChinese(length) {
+
+        let randomChinese = '';
+
+        let count = length;
+
+        while ( count >= 1 ) {
+
+            const sign = Number(String(Date.now()).substring(String(Date.now()).length - 5, String(Date.now()).length));
+
+            randomChinese += String.fromCharCode(sign);
+
+            setTimeout(count--, 50);
+
+        }
+
+        return randomChinese
+    }
+    
+    console.log(getRandomChinese(5));
+    
 });
