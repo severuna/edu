c09e73a642fa1939d848dca0ee2a8cc9a77996c8
Severuna <oxxxyfomenkova@gmail.com>
2023-04-10 22:21:02 +0300

    set forms processing & output value (console)

--- homework_12/script/index.js
+++ homework_12/script/index.js
@@ -4,6 +4,30 @@ document.addEventListener("DOMContentLoaded", ( ) => {
 
     const charactersForm = document.forms.showCharacters;
 
+    charactersForm.addEventListener("submit", ( e )=> {
+
+        e.preventDefault();
+
+        console.log('charactersForm submit');
+
+        const characterInput = charactersForm[0].value;
+
+        console.log(`You have chosen movie number ${ characterInput }`);
+
+    });
+
     const planetsForm = document.forms.showPlanets;
+
+    planetsForm.addEventListener("submit", ( e )=> {
+
+        e.preventDefault();
+
+        console.log('planetsForm submit');
+
+        const planetInput = planetsForm[0].value;
+
+        console.log(`You have chosen movie number ${ planetInput }`);
+        
+    });
     
 });
