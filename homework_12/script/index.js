7059e016a87e41f55370d1290f98b7d0cbfdd407
Severuna <oxxxyfomenkova@gmail.com>
2023-04-10 22:15:13 +0300

    call output container

--- homework_12/script/index.js
+++ homework_12/script/index.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", ( ) => {
 
+    const output = document.querySelector("#output");
+
     const charactersForm = document.forms.showCharacters;
-    
+
     const planetsForm = document.forms.showPlanets;
     
 });
