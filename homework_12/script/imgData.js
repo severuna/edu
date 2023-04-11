adb7ea4a4dffa9f798dbd1665ef918faf859a491
Severuna <oxxxyfomenkova@gmail.com>
2023-04-11 23:24:18 +0300

    set img data ( 7 pages swapi people & img src )

--- homework_12/script/imgData.js
+++ homework_12/script/imgData.js
@@ -1,8 +1,367 @@
 const imgData = [
 
     {
+        name: 'Luke Skywalker',
+        src: 'https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-large-heroes-page-luke-skywalker-16x9-xl.jpg.adapt.crop1x1.320w.jpg'
+    },
+
+    {
+        name: 'C-3PO',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/3/35/C-3PO.png/'
+    },
+
+    {
+        name: 'R2-D2',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/1/1a/R2d2.jpg/'
+    },
+
+    {
+        name: 'Darth Vader',
+        src: 'https://static.wikia.nocookie.net/villains/images/7/76/Darth_Vader.jpg/'
+    },
+
+    {
+        name: 'Leia Organa',
+        src: 'https://pm1.narvii.com/6690/c9dfdff22abbc0b755756a6e8c5e2d5ea220f605_hq.jpg'
+    },
+
+    {
+        name: 'Owen Lars',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/8/81/Owen-OP.jpg/'
+    },
+    
+    {
+        name: 'Beru Whitesun lars',
+        src: 'https://static.wikia.nocookie.net/starwars/images/8/84/BeruWhitesunLars.jpg/'
+    },
+    
+    {
+        name: 'R5-D4',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/2/2c/R5d4.jpg/'
+    },
+    
+    {
+        name: 'Biggs Darklighter',
+        src: 'https://www.starwars-union.de/bilder/lexikon/20060819Biggs.jpg'
+    },
+    
+    {
+        name: 'Obi-Wan Kenobi',
+        src: 'https://cdnb.artstation.com/p/assets/images/images/032/846/253/4k/adam-o-donnell-final.jpg'
+    },
+    
+    {
+        name: 'Anakin Skywalker',
+        src: 'https://static.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/'
+    },
+    
+    {
+        name: 'Wilhuff Tarkin',
+        src: 'https://static.wikia.nocookie.net/starwars/images/9/95/Tarkin-SWG4.jpg/'
+    },
+    
+    {
+        name: 'Chewbacca',
+        src: 'https://static.wikia.nocookie.net/starwars/images/e/ec/ChewbaccaCSWE.jpg/'
+    },
+    
+    {
+        name: 'Han Solo',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/f/fc/HanChillinAtChalmuns-ANH.jpg/'
+    },
+    
+    {
+        name: 'Greedo',
+        src: 'https://static.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg/'
+    },
+    
+    {
+        name: 'Jabba Desilijic Tiure',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Jabba_the_Hutt.jpg'
+    },
+    
+    {
+        name: 'Wedge Antilles',
+        src: 'https://static.wikia.nocookie.net/starwars/images/7/7e/WedgesEntireHead-ROTJ.png/'
+    },
+    
+    {
+        name: 'Jek Tono Porkins',
+        src: 'https://static.wikia.nocookie.net/starwars/images/e/eb/JekPorkins-DB.png/'
+    },
+    
+    {
+        name: 'Yoda',
+        src: 'https://static.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png/'
+    },
+    
+    {
+        name: 'Palpatine',
+        src: 'https://static.wikia.nocookie.net/starwars/images/d/d8/Emperor_Sidious.png/'
+    },
+    
+    {
+        name: 'Boba Fett',
+        src: 'https://upload.wikimedia.org/wikipedia/en/3/3e/FettbobaJB.png'
+    },
+    
+    {
+        name: 'IG-88',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/a/aa/IG-88B-ESB.jpg/'
+    },
+    
+    {
+        name: 'Bossk',
+        src: 'https://static.wikia.nocookie.net/starwars/images/1/1d/Bossk.png/'
+    },
+    
+    {
+        name: 'Lando Calrissian',
+        src: 'https://static.wikia.nocookie.net/star-wars-canon-extended/images/0/0a/Lando5.jpg/'
+    },
+    
+    {
+        name: 'Lobot',
+        src: 'https://static.wikia.nocookie.net/starwars/images/0/0e/Lobot_SWDR.png/'
+    },
+    
+    {
+        name: 'Ackbar',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/f/fb/Ackbar_HS.jpg/'
+    },
+    
+    {
+        name: 'Mon Mothma',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/4/46/Monmothma.jpg/'
+    },
+    
+    {
+        name: 'Arvel Crynyd',
+        src: 'https://ayay.co.uk/backgrounds/star_wars/rebel_alliance_characters/arvel-crynyd.jpg'
+    },
+    
+    {
+        name: 'Wicket Systri Warrick',
+        src: 'https://static.wikia.nocookie.net/starwarsofthecaribbean/images/0/06/Wicket_wosw.jpg/'
+    },
+    
+    {
+        name: 'Nien Nunb',
+        src: 'https://static.wikia.nocookie.net/starwars/images/4/46/NienNunbHS-SWE.png/'
+    },
+    
+    {
+        name: 'Qui-Gon Jinn',
+        src: 'https://static.wikia.nocookie.net/swfanon/images/d/d6/QuiGonPortrait-SWE.jpg/'
+    },
+    
+    {
+        name: 'Nute Gunray',
+        src: 'https://static.wikia.nocookie.net/swfanon/images/1/14/Nute_Gunray_Commands.jpg/'
+    },
+    
+    {
+        name: 'Finis Valorum',
+        src: 'https://i.pinimg.com/originals/29/16/62/29166227119bf6376119c9d89c3d88c7.jpg'
+    },
+    
+    {
+        name: 'Padmé Amidala',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/c/cb/%D0%9F%D0%B0%D0%B4%D0%BC%D0%B5_%D0%90%D0%BC%D0%B8%D0%B4%D0%B0%D0%BB%D0%B0.png/'
+    },
+    
+    {
+        name: 'Jar Jar Binks',
+        src: 'https://upload.wikimedia.org/wikipedia/en/4/4b/Jjportrait.jpg'
+    },
+    
+    {
+        name: 'Roos Tarpals',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/c/ca/%D0%A0%D1%83%D1%81_%D0%A2%D0%B0%D1%80%D0%BF%D0%B0%D0%BB%D1%8C%D1%81.jpg/'
+    },
+    
+    {
+        name: 'Rugor Nass',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/8/86/Bossnas.jpg/'
+    },
+    
+    {
+        name: 'Ric Olié',
+        src: 'https://static.wikia.nocookie.net/starwars/images/4/4c/RicOlieHS-SWE.png/'
+    },
+    
+    {
+        name: 'Watto',
+        src: 'https://static.wikia.nocookie.net/starwars/images/e/eb/WattoHS.jpg/'
+    },
+    
+    {
+        name: 'Sebulba',
+        src: 'https://i.pinimg.com/originals/8a/90/dc/8a90dcda66d0da09fa1a74bab9af33d2.jpg'
+    },
+    
+    {
+        name: 'Quarsh Panaka',
+        src: 'https://static.wikia.nocookie.net/headhuntersholosuite/images/6/65/Panaka.jpg/'
+    },
+    
+    {
+        name: 'Shmi Skywalker',
+        src: 'https://static.wikia.nocookie.net/starwars/images/6/6c/ShmiSkywalkerLars-Databank.jpg/'
+    },
+    
+    {
+        name: 'Darth Maul',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/8/86/Darth_Maul_Cosplayer_at_MCM_Comic_Con_October_2016.jpg'
+    },
+    
+    {
+        name: 'Bib Fortuna',
+        src: 'https://static.wikia.nocookie.net/awakening-of-the-rebellion/images/3/33/BibFortunaHS-ROTJ.png/'
+    },
+    
+    {
+        name: 'Ayla Secura',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/4/4d/Aayla_Secura_SWE.png/'
+    },
+    
+    {
+        name: 'Ratts Tyerel',
+        src: 'https://rpggamer.org/uploaded_images/RattsHS.jpg'
+    },
+    
+    {
+        name: 'Dud Bolt',
+        src: 'https://s3.eu-west-2.amazonaws.com/ourartcorner/20190430/6c9dc7-DUD-BOLT-max.jpeg'
+    },
+    
+    {
+        name: 'Gasgano',
+        src: 'https://static.wikia.nocookie.net/starwars/images/a/a4/GasganoHS-SWE.png'
+    },
+    
+    {
+        name: 'Ben Quadinaros',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/6/62/Ben_quadinaros2.jpg/'
+    },
+    
+    {
+        name: 'Mace Windu',
+        src: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Mace_Windu.png'
+    },
+    
+    {
+        name: 'Ki-Adi-Mundi',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/9/9e/KiAdiMundi.jpg/'
+    },
+    
+    {
+        name: 'Kit Fisto',
+        src: 'https://static.wikia.nocookie.net/starwars/images/2/25/KitFisto-USWNE.png/'
+    },
+    
+    {
+        name: 'Eeth Koth',
+        src: 'https://static.wikia.nocookie.net/starwars/images/4/4e/EethKothCardTrader.png/'
+    },
+    
+    {
+        name: 'Adi Gallia',
+        src: 'https://static.wikia.nocookie.net/starwars/images/f/f2/AdiGallia2-SWE.png/'
+    },
+    
+    {
+        name: 'Saesee Tiin',
+        src: 'https://static.wikia.nocookie.net/starwars/images/a/a3/SaeseeTiin-SWCT.png/'
+    },
+    
+    {
+        name: 'Yarael Poof',
+        src: 'https://static.wikia.nocookie.net/starwars/images/9/96/Yarael_Poof.png/'
+    },
+    
+    {
+        name: 'Plo Koon',
+        src: 'https://static.wikia.nocookie.net/starwars/images/8/89/Guard_SWD_2017_Q3_Tournament_Kit.png/'
+    },
+    
+    {
+        name: 'Mas Amedda',
+        src: 'https://static.wikia.nocookie.net/starwars/images/3/37/Mas_Amedda_SWCT.png/'
+    },
+    
+    {
+        name: 'Gregar Typho',
+        src: 'https://static.wikia.nocookie.net/starwars/images/d/d3/CaptainTypho-SWCT.png/'
+    },
+    
+    {
+        name: 'Cordé',
+        src: 'https://static.wikia.nocookie.net/starwars/images/6/6c/Corde-ChroniclesPrequels.jpg/'
+    },
+    
+    {
+        name: 'Cliegg Lars',
+        src: 'https://static.wikia.nocookie.net/starwars/images/3/36/ClieggLarsHS-SWE.jpg/'
+    },
+    
+    {
+        name: 'Poggle the Lesser',
+        src: 'https://static.wikia.nocookie.net/starwars/images/9/93/Poggle_the_lesser_-_sw_card_trader.png/'
+    },
+    
+    {
+        name: 'Luminara Unduli',
+        src: 'https://static.wikia.nocookie.net/starwars/images/b/ba/Luminarafull.jpg/'
+    },
+    
+    {
+        name: 'Barriss Offee',
+        src: 'https://static.wikia.nocookie.net/starwars/images/a/a4/BarrissOffee-OP.png/'
+    },
+    
+    {
+        name: 'Dormé',
+        src: 'https://static.wikia.nocookie.net/rustarwars/images/c/c9/%D0%94%D0%BE%D1%80%D0%BC%D0%B5%D0%AD%D0%BF2.jpg/'
+    },
+    
+    {
+        name: 'Dooku',
+        src: 'https://static.wikia.nocookie.net/villains/images/f/f5/Count-dooku.jpg/'
+    },
+    
+    {
+        name: 'Bail Prestor Organa',
+        src: 'https://static.wikia.nocookie.net/starwars/images/b/b0/Bailrogueone.jpg/'
+    },
+    
+    {
+        name: 'Jango Fett',
+        src: 'https://static.wikia.nocookie.net/starwars/images/0/03/Jango_OS_TPB.png/'
+    },
+    
+    {
+        name: 'Zam Wesell',
+        src: 'https://comicvine.gamespot.com/a/uploads/original/1/15317/322749-47126-zam-wesell.jpg'
+    },
+    
+    {
+        name: 'Dexter Jettster',
+        src: 'https://static.wikia.nocookie.net/swfanon/images/8/82/Dexter.jpg/'
+    },
+
+    // stop
+    // next link - https://swapi.dev/api/people/?page=8
+    
+    {
         name: '',
         src: ''
-    }
+    },
+    
+    {
+        name: '',
+        src: ''
+    },
+
 
 ];
