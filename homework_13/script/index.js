c87fa63a340fb8dfa3cf4afbd1f2c778d4c3ca1a
Severuna <oxxxyfomenkova@gmail.com>
2023-04-15 21:05:50 +0300

    set createIdGenerator func logic & set var idGenerator

--- homework_13/script/index.js
+++ homework_13/script/index.js
@@ -3,6 +3,14 @@
 // idGenerator.next().value -> 2
 // idGenerator.next().value -> 3
 
+function* createIdGenerator() {
+    let i = 1;
+    while(true)
+        yield i++;
+}
+
+const idGenerator = createIdGenerator();
+
 const nextOutput = document.querySelector("#nextOutput");
 
 const nextBtn = document.querySelector("#nextBtn");
