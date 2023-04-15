43c47d67943b354994a3d3de83bb84ea61a39fc7
Severuna <oxxxyfomenkova@gmail.com>
2023-04-15 20:55:36 +0300

    call nextOutput container

---
+++ homework_13/script/index.js
@@ -0,0 +1,6 @@
+// 1 Створіть нескінченний генератор ідентифікаторів. Повинен працювати наступним чином:
+// idGenerator.next().value -> 1
+// idGenerator.next().value -> 2
+// idGenerator.next().value -> 3
+
+const nextOutput = document.querySelector("#nextOutput");
