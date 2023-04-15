c54a8cf9b1b8e1a86bfccb6c5df2024ec51b21ec
Severuna <oxxxyfomenkova@gmail.com>
2023-04-15 21:10:46 +0300

    set showData func logic ( for view res dom )

--- homework_13/script/index.js
+++ homework_13/script/index.js
@@ -1,3 +1,15 @@
+function showData ( container, task, answer ) {
+
+    const item = document.createElement("span");
+
+    item.textContent = `${ task } -> ${ answer }`;
+
+    container.append(item);
+
+    return container
+
+}
+
 // 1 Створіть нескінченний генератор ідентифікаторів. Повинен працювати наступним чином:
 // idGenerator.next().value -> 1
 // idGenerator.next().value -> 2
@@ -18,7 +30,5 @@ const nextBtn = document.querySelector("#nextBtn");
 nextBtn.addEventListener("click", ( e ) => {
 
     e.preventDefault();
-
-    console.log('click nextBtn');
 
 });
