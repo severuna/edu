a39190d5ad5748e3cbd6987a81ecb503cc24eecf
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 15:14:33 +0300

    create answerDataTwo

--- homework_15/script/mvc/answerData/answerDataTwo.js
+++ homework_15/script/mvc/answerData/answerDataTwo.js
@@ -1,39 +1,25 @@
-
+// 1
 let N;
 
 do {
     N = +prompt('Введіть перше число, будь ласка. Воно повинно дорівнювати або бути більше 0.');
 } while ( N < 0 || !Number.isInteger(N) );
 
-
-console.log(`Перше число, що введене користвачем, - ${ N }.`);
-document.querySelector("#first").textContent = N;
-
-
+// 2
 const numCheckMessage = `Число нам підходить.`;
 
-console.log( numCheckMessage );
-document.querySelector("#second").textContent = numCheckMessage;
-
+// 3
 let M;
 
 do {
     M = +prompt(`Введіть друге число, будь ласка. Воно повинно бути більше першого числа (перше число, що ви ввели, - ${ N }).`);
 } while ( M < N || !Number.isInteger(M) );
 
-console.log(`Друге число, що введене користвачем, - ${ M }.`);
-document.querySelector("#third").textContent = M;
-
-console.log( numCheckMessage );
-document.querySelector("#four").textContent = numCheckMessage;
-
+// 5
 const evenNumCheck = confirm('Чи враховувати парні числа? ОК - так, Cancel - ні.');
-
 const evemNumMessage = `Ви обрали - ${ evenNumCheck }, тому числа ${ evenNumCheck ? ' будуть ' : ' не будуть' } враховані.`;
 
-document.querySelector("#five").textContent = evemNumMessage;
-console.log( evemNumMessage );
-
+// 6
 let sum = 0;
 
 for ( let i = N ; i <= M ; i ++ ) {
@@ -46,5 +32,41 @@ for ( let i = N ; i <= M ; i ++ ) {
     }
 }
 
-document.querySelector("#six").textContent = sum;
-console.log (`Сума обраних чисел  ${ sum }`);
+const answerDataTwo = [
+    {
+        id: 1,
+        answer: [
+            N
+        ]
+    },
+    {
+        id: 2,
+        answer: [
+            numCheckMessage
+        ]
+    },
+    {
+        id: 3,
+        answer: [
+            M
+        ]
+    },
+    {
+        id: 4,
+        answer: [
+            numCheckMessage
+        ]
+    },
+    {
+        id: 5,
+        answer: [
+            evemNumMessage
+        ]
+    },
+    {
+        id: 6,
+        answer: [
+            sum
+        ]
+    }
+]
