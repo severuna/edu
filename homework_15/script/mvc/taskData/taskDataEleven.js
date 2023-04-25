fe13dff7869665d471db3235db98497dd373684d
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 18:05:17 +0300

    create taskDataEleven

---
+++ homework_15/script/mvc/taskData/taskDataEleven.js
@@ -0,0 +1,21 @@
+const taskDataEleven = [
+    {
+        id: 1,
+        txt: `Створіть функцію, яка повертає проміс getRandomChinese(length).
+        <br>
+        Функція працює таким чином:
+        <br>
+        Запускається цикл (підказка: можна використовувати while) length раз, кожен прохід циклу дивимось на останні 5 цифр результату виклику методу Date.now()
+        <br>
+        Наприклад отримали const sign = 16086;.
+        <br>
+        Конвертуємо отриману цифу в рядок String.fromCharCode(sign); та чекаємо 50 ms
+        <br>
+        Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами.
+        <br>
+        Час роботи проміса має складати length * 50ms.
+        `
+    }
+];
+
+export default taskDataEleven;
