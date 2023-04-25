8c9002e3750269767139efcc54505008568080b9
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 14:16:50 +0300

    import getElement

--- homework_15/script/index.js
+++ homework_15/script/index.js
@@ -1,6 +1,7 @@
 console.log('the page is loaded');
 
 import formHandler from "./handler/formHandler.js";
+import getElement from "./handler/getElement.js";
 import showWork from "./show/showWork.js";
 import showTaskContainer from "./show/showTaskContainer.js";
 import showTask from "./show/showTask.js";
