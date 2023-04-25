919012a7949bc0773e8d3bd92280c2e199d263b0
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 18:48:03 +0300

    set work 8 data arr

--- homework_15/script/mvc/workData/eight.js
+++ homework_15/script/mvc/workData/eight.js
@@ -101,15 +101,17 @@ const userStudentDismiss = `відраховано`;
 // 7
 const userStudentRecover = `поновлено`;
 
-const answerDataEight = [
+const taskDataEight = [
     {
         id: 1,
+        txt: `У цьому завданні вам необхідно створити клас <span>Student</span> та розробляти методи всередині цього класу. У стдентів повинні бути наступні властивості: <span>university, course, fullName</span>, вони передаються при створенні студента(в конструктор).`,
         answer: [
             classStudent
         ]
     },
     {
         id: 2,
+        txt: `Створіть метод <span>this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії м.Одеса, Остап Родоманський Бендер"</span>, метод повертає сукупну інформацію про курс, учбовий заклад та імені студента.`,
         answer: [
             userStudent.fullName,
             userStudent.course,
@@ -118,34 +120,39 @@ const answerDataEight = [
     },
     {
         id: 3,
+        txt: `Створіть геттер оцінок <span>this.marks</span>, який повертає масив оцінок студента <span>[5, 4, 4, 5]</span>`,
         answer: [
             user.marks
         ]
     },
     {
         id: 4,
+        txt: `Створіть сеттер оцінок <span>this.marks = 5</span>, який дозволяє поставити оцінку студенту. Після того, як оцінка поставлена, геттер повинен повернути масив <span>this.marks -> [5, 4, 4, 5, 5]</span>`,
         answer: [
             user.marks
         ]
     },
     {
         id: 5,
+        txt: `Створіть метод отримання середнього балу <span>this.getAverageMark() -> 4.6</span>`,
         answer: [
             averageMark
         ]
     },
     {
         id: 6,
+        txt: `Створіть метод <span>this.dismiss</span>, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких помилок, просто повертається завжди <span>null</span> замість масиву оцінок)`,
         answer: [
             userStudentDismiss
         ]
     },
     {
         id: 7,
+        txt: `Створіть метод <span>this.recover</span>, який дозволить поновити студента`,
         answer: [
             userStudentRecover
         ]
     }
 ];
 
-export default answerDataEight;
+export default taskDataEight;
