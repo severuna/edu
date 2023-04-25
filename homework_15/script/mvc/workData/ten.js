091798179aa1005bbf838cd84415dcabc68be54f
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 18:59:30 +0300

    set work 10 data arr

--- homework_15/script/mvc/workData/ten.js
+++ homework_15/script/mvc/workData/ten.js
@@ -22,13 +22,14 @@ const play = document.addEventListener("DOMContentLoaded", ( ) => {
     
 });
 
-const answerDataTen = [
+const taskDataTen = [
     {
         id: 1,
+        txt: `Необхідно написати програму, яка буде зчитувати з клавіатури натискання клавіш та відтворювати унікальний звук для кожної клавіши (клавіши можуть бути будь-які, звуки можуть бути будь-які, тільки вкажіть у верстці – які кнопки натискати).`,
         answer: [
             play
         ]
     }
 ];
 
-export default answerDataTen;
+export default taskDataTen;
