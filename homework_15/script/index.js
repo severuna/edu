ea265186b7c8e27894da82799407bedddc9d37bf
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 13:55:23 +0300

    import show task, answer etc

--- homework_15/script/index.js
+++ homework_15/script/index.js
@@ -1,6 +1,11 @@
 console.log('the page is loaded');
 
 import formHandler from "./handler/formHandler.js";
+import showWork from "./show/showWork.js";
+import showTaskContainer from "./show/showTaskContainer.js";
+import showTask from "./show/showTask.js";
+import showAnswer from "./show/showAnswer.js";
+
 
 const worksForm = document.forms.worksForm;
 
