0ffc702ac0021922a7fbe433da99c3f23cf7205e
Severuna <oxxxyfomenkova@gmail.com>
2023-04-25 18:57:17 +0300

    set work 9 data arr

--- homework_15/script/mvc/workData/nine.js
+++ homework_15/script/mvc/workData/nine.js
@@ -1,3 +1,25 @@
+const blocks = ( ) => {
+
+    const root = document.querySelector("#root");
+
+    const generateBlocks = ( container ) => {
+        for ( let i = 0; i < 25 ; i++ ) {
+            const block = document.createElement("div");
+            block.classList.add("block");
+            container.append(block);
+        }
+    }
+    generateBlocks(root);
+
+    (function generateBlocksInterval( ) {
+        [...document.querySelectorAll(".block")].forEach( block => {
+            block.style.backgroundColor = `rgb(${Math.floor( Math.random() * ( 250 - 0 ) + 0 )}, ${Math.floor( Math.random() * ( 250 - 0 ) + 0 )}, ${Math.floor( Math.random() * ( 250 - 0 ) + 0 )})`;
+        });
+        setTimeout(generateBlocksInterval, 1000);
+    })();
+
+}
+
 const taskDataNine = [
     {
         id: 1,
@@ -8,7 +30,10 @@ const taskDataNine = [
         Зробіть так, щоб квадрати змінювали колір раз на секунду.
         <br>
         Щоб квадрати з'явились на сторінці та почали змінюватись, необхідно викликати функцію <span>generateBlocksInterval();</span>.
-        `
+        `,
+        answer: [
+            blocks
+        ]
     }
 ];
 
