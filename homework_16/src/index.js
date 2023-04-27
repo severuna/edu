08217a393edb4951dd65463e7934e4dff0d69185
Severuna <oxxxyfomenkova@gmail.com>
2023-04-27 23:55:21 +0300

    create user data

--- homework_16/src/index.js
+++ homework_16/src/index.js
@@ -8,6 +8,17 @@ import comment from './img/comment.svg';
 import download from './img/download.svg';
 import retweet from './img/retweet.svg';
 
+const user = {
+  name: "Anakin Skywalker",
+  avatar: anakin_skywalker,
+  nickname: "dart_vader",
+  posts: {
+    date: "26.02.2019",
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?"",
+    media: ray_skywalker
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <h1>Init title</h1>
