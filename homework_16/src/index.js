504541d5a9b0508db9f9e28a99cbea0c875a69dd
Severuna <oxxxyfomenkova@gmail.com>
2023-04-28 00:26:24 +0300

    set main markup

--- homework_16/src/index.js
+++ homework_16/src/index.js
@@ -12,14 +12,35 @@ const user = {
   name: "Anakin Skywalker",
   avatar: anakin_skywalker,
   nickname: "dart_vader",
-  posts: {
-    date: "26.02.2019",
-    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
-    media: ray_skywalker
-  }
+  date: "26.02.2019",
+  content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+  media: ray_skywalker
+}
+
+function Post ( ) {
+  return (
+    <div className='post flex-column'>
+      <div className='post-head flex-row'>
+        <img className='post-head__avatar' src={user.avatar} alt="avatar" />
+        <h1 className='post-head__name'>{user.name}</h1>
+        <p className='post-head__nickname'>{user.nickname}</p>
+        <p className='post-head__date'>{user.date}</p>
+      </div>
+      <div className='post-content flex-column'>
+        <p className='post-content__text'>{user.content}</p>
+        <img className='post-content__media' src={user.media} alt='post media' />
+        <div className='post-content__reaction flex-row'>
+          <img src={comment} alt="comment pic" />
+          <img src={retweet} alt="retweet pic" />
+          <img src={like} alt="like pic" />
+          <img src={download} alt="download pic" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Init title</h1>
+  <Post />
 );
