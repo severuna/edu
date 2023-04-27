b692442f55e5972c4bfa788f098b2265a9940cc9
Severuna <oxxxyfomenkova@gmail.com>
2023-04-27 23:48:00 +0300

    import imgs to index.js

--- homework_16/src/index.js
+++ homework_16/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import anakin_skywalker from './img/anakin_skywalker.jpg';
+import ray_skywalker from './img/ray_skywalker.jpg';
+import like from './img/like.svg';
+import comment from './img/comment.svg';
+import download from './img/download.svg';
+import retweet from './img/retweet.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
